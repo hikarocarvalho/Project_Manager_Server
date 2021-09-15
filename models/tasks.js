7e49baa87f7a2d21254febe496a79db82581e45f
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-09-15 17:16:19 -0300

    almost done

--- models/tasks.js
+++ models/tasks.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const tasksModel = new mongoose.Schema({
     task_name: { type: String, required: true },
+    task_description: { type: String, required: true },
     task_priority: { type: String, default:"Low",required:true},
     task_final_date: {type: Date, required:true},
     project_id: { type: String, required: true },
